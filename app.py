93d523e9bcfc164795c5c649b84aa2343b019402
saidmtanzania <saidmtanzania@gmail.com>
2023-08-17 14:02:05 +0300

    updated

--- app.py
+++ app.py
@@ -1,80 +1,28 @@
-from flask import Flask, request, Response
-import pdfplumber
+from flask import Flask, request, jsonify
+import pandas as pd
+
+# Load your data frame from a CSV file
+data = pd.read_csv('./res/Student.csv')
 
 app = Flask(__name__)
 
+@app.route('/get_message', methods=['POST'])
+def get_message():
+    request_data = request.json  # Assuming you are sending JSON data
 
-@app.route('/pdf/', methods=['POST'])
-def add_pdf():
-    pdf1 = request.files.get('file1').read()
-    pdf2 = request.files.get('file2').read()
+    registration_number = request_data.get('registration_number')
 
-    file_name1 = request.form.get('file1Name')
-    file_name2 = request.form.get('file2Name')
+    if registration_number is None:
+        return jsonify(message='Missing registration_number in request'), 400
 
-    with open(f'./pdf/{file_name1}', 'wb') as f:
-        f.write(pdf1)
-    with open(f'./pdf/{file_name2}', 'wb') as f:
-        f.write(pdf2)
+    # Search for the registration number in your data frame
+    result = data[data['RegistrationNumber'] == registration_number]
 
-    result = []
-    with pdfplumber.open(f'./pdf/{file_name1}') as pdf:
-        for i in range(0, len(pdf.pages)):
-            page = pdf.pages[i]
-            text = page.extract_text(x_tolerance=1)
-            results = " " + text
-            result.append((results))
-
-    string_results = "".join(result)
-    lines = string_results.split('\n')
-    data = []
-    for ele in lines:
-        if ele.strip():
-            data.append(ele)
-    student_reg_phone = []
-    for i in range(0, len(data)):
-        student = data[i].split(sep=None, maxsplit=-1)
-        student_reg_phone.append((student))
-    result = []
-    with pdfplumber.open(f'./pdf/{file_name2}') as pdf:
-        for i in range(0, len(pdf.pages)):
-            page = pdf.pages[i]
-            text = page.extract_text(x_tolerance=1)
-            results = " " + text
-            result.append((results))
-    string_results = "".join(result)
-
-    lines = string_results.split('\n')
-    data = []
-    for ele in lines:
-        if ele.strip():
-            data.append(ele)
-    
-    student_info = []
-    for i in range(0, len(data)):
-        student = data[i].split(sep=None, maxsplit=-1)
-        student_info.append((student[-3:]))
-    import pandas as pd
-    data = pd.DataFrame(student_info, columns=['RegNumber', 'Year_study', 'Ada'])
-    data = data.replace(',', '', regex=True)
-    reg_phone = pd.DataFrame(student_reg_phone, columns=['RegNumber', 'phone_number'])
-    data.insert(3, column='page_number', value='Nan')
-    new_page = data[data['Ada'].isna()].index.tolist()
-    new_page.insert(0, 0)
-
-    for i in range(0, (len(new_page) - 1)):
-        data['page_number'][new_page[i]:new_page[i + 1]] = data['RegNumber'][
-            new_page[i + 1]]
-    data['Message'] = 'Registration number:' + data['RegNumber'] + ',' + 'Ada :' + data['Ada'] + ',' + 'page number: ' + data['page_number'].astype(str)
-    result = data.merge(reg_phone, on='RegNumber')
-    result = result.drop(['RegNumber', 'Ada', 'Year_study', 'page_number'],axis=1)
-    result = result.dropna()
-
-    result.to_json("./res/Result45.json", orient='records')
-   
-    return Response(result.to_json(orient='records'), mimetype='application/json')
-
-
+    if result.empty:
+        return jsonify(message='Registration number not found'), 404
+    else:
+        message = result.iloc[0]['Message']
+        return jsonify(message=message), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
